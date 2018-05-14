6922f27884763877b4a300385154fa4fc014b565
Mariko <mariko9012@gmail.com>
2018-05-14 16:59:47 +0900

    Generated files

---
+++ docs/posts/testpost/data.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: 'Test post',
+	date: '5/9/1991',
+	tags: ['urbanism'],
+	description: 'description',
+	mapPosition: {
+		bearing: 0,
+		center: [139.905913, 35.712899],
+		zoom: 4.34,
+		pitch: 0
+	}
+}
---
+++ docs/posts/text-4/data.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: 'test-4',
+	date: '12/12/2017',
+	tags: ['Tokyo'],
+	description: 'description',
+	mapPosition: {
+		bearing: 0,
+		center: [139.905913, 35.712899],
+		zoom: 4.34,
+		pitch: 0
+	}
+}
