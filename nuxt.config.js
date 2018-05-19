e773b65429a121ca336e2a9effbe099fae3f09ed
Jasper Stephenson <jasperstephenson@gmail.com>
2018-05-19 09:34:10 -0500

    Changed page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 const posts = require('./static/posts.json')
 module.exports = {
   head: {
-    title: 'myfirstfunnuxtproject',
+    title: 'Traveling Circus of Urbanism',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
