2175525e091cd0fa2d1eb82c0434d18ab866c8bf
Mariko <mariko9012@gmail.com>
2018-05-28 17:07:45 -0500

    post update

---
+++ static/posts/tokyo/HackMyCity/data.js
@@ -0,0 +1,11 @@
+module.exports = {
+	date: '5/9/1991',
+	location: '100Banch',
+	category: 'essay',
+	mapPosition: {
+		bearing: 0,
+		center: [139.7047394, 35.6559546],
+		zoom: 12.00,
+		pitch: 0
+	}
+}
