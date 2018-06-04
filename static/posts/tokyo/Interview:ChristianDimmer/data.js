f886ff8bed4507ebf3066652bc38d4f6cb545b72
Mariko <mariko9012@gmail.com>
2018-06-04 15:31:51 -0500

    post update

--- static/posts/tokyo/Interview:ChristianDimmer/data.js
+++ static/posts/tokyo/Interview:ChristianDimmer/data.js
@@ -1,9 +1,8 @@
 module.exports = {
-	public: false,
-	date: '3/13/2018',
+	draft: true,
+	date: '11/24/2016',
 	category: 'essay',
 	mapPosition: {
-		location: '100Banch',
 		bearing: 0,
 		center: [139.7047394, 35.6559546],
 		zoom: 12.00,
