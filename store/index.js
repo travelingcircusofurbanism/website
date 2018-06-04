16112a7cf177f6c52a273f4133a8b79c6ab0b874
Jasper Stephenson <jasperstephenson@gmail.com>
2018-06-04 16:02:59 -0500

    setMapMarkers is EVEN MORE bulletproof woo!

--- store/index.js
+++ store/index.js
@@ -8,7 +8,8 @@ export default () => {
     },
     mutations: {
       setMapMarkers (state, newMarkers) {
-        if (!newMarkers || newMarkers.length === 0) return
+        console.log(newMarkers)
+        if (!newMarkers || newMarkers.length === 0 || Object.keys(newMarkers).length === 0) return
         state.highlightedLocation = null   
         state.mapMarkers = (newMarkers.center) ?
           // catch a single post's mapPosition object
@@ -16,23 +17,28 @@ export default () => {
             position: newMarkers,
             locationName: newMarkers.location
           }] :
-          (newMarkers[0].position) ?
-          // hard-coded marker data, use as-is
-          newMarkers :
-          // or, pull data from post objects
-          newMarkers
-            .map(post => (Array.isArray(post.mapPosition)) ?
-              post.mapPosition.map(position => ({
-                position: { ...position },
-                locationName: position.location
-              })) :
-              [{
-                position: { ...post.mapPosition },
-                locationName: post.mapPosition.location
-              }]
-            ).reduce((acc, curr) =>
-              acc.concat(curr)
-            , [])
+          //or, a single post's mapPosition array
+          (newMarkers[0].center) ?
+            newMarkers.map(pos => ({
+              position: pos,
+              locationName: pos.location
+            })):
+            (newMarkers[0].position) ?
+            // hard-coded marker data, use as-is
+            newMarkers :
+            // or, pull data from post objects
+            newMarkers
+              .map(post => (Array.isArray(post.mapPosition)) ?
+                post.mapPosition.map(position => ({
+                  position: { ...position },
+                  locationName: position.location
+                })) :
+                [{
+                  position: { ...post.mapPosition },
+                  locationName: post.mapPosition.location
+                }]
+              )
+              .reduce((acc, curr) => acc.concat(curr), [])
       },
       setHighlight (state, location) {
         state.highlightedLocation = location
