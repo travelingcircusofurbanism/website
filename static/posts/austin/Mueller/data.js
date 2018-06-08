e4285b55da00331a4b1ef7263e79c3824d2f2d68
Mariko <mariko9012@gmail.com>
2018-06-08 13:58:22 -0600

    post update

--- static/posts/austin/Mueller/data.js
+++ static/posts/austin/Mueller/data.js
@@ -1,9 +1,9 @@
 module.exports = {
 	public: true,
 	date: '5/28/2018',
-	location: 'Mueller',
 	category: 'place',
 	mapPosition: {
+		location: 'Mueller',
 		bearing: 0,
 		center: [-97.70094159999996, 30.29841529999999],
 		zoom: 12.00,
