91d3e29a4db22537a6df8ca982ebb30b3be76a99
Mariko <mariko9012@gmail.com>
2018-06-25 19:49:48 -0700

    Auto-pushing files to live

---
+++ static/posts/arcosanti/arconsanti/data.js
@@ -0,0 +1,11 @@
+module.exports = {
+	public: false,
+	date: '4/13/2015',
+	category: 'place',
+	mapPosition: {
+		bearing: 0,
+		center: [16.37381890000006, 48.2081743],
+		zoom: 12.00,
+		pitch: 0
+	}
+}
