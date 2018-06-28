87cd66afe4050a717d5cf0ec1171886b092914a1
Mariko <mariko9012@gmail.com>
2018-06-27 22:24:14 -0700

    Auto-pushing files to live

--- docs/posts/arcosanti/arconsanti/data.js
+++ docs/posts/arcosanti/arconsanti/data.js
@@ -4,6 +4,7 @@ module.exports = {
 	category: 'place',
 	image: 'arcosanti00.jpg',
 	mapPosition: {
+		location: 'Arcosanti',
 		bearing: 0,
 		center: [-112.10055469999998, 34.3425315],
 		zoom: 12.00,
