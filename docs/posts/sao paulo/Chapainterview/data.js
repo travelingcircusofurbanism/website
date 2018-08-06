58fd9b0f6b6d0fc8135c518feafab9ccb793e9c6
Mariko <mariko9012@gmail.com>
2018-08-06 11:49:54 -0700

    Auto-pushing files to live

---
+++ docs/posts/sao paulo/Chapainterview/data.js
@@ -0,0 +1,12 @@
+module.exports = {
+	public: false,
+	date: '6/03/2018',
+	category: 'interview',
+	mapPosition: {
+		location: 'Sao Paulo',
+		bearing: 0,
+		center: [-46.63330939999997, -23.5505199],
+		zoom: 12.00,
+		pitch: 0
+	}
+}
