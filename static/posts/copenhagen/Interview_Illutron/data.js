0151b737ad3670355f0ea9ad37bbd1c331067ca8
Mariko <mariko9012@gmail.com>
2018-08-16 11:06:27 -0700

    Auto-pushing files to live

---
+++ static/posts/copenhagen/Interview_Illutron/data.js
@@ -0,0 +1,12 @@
+module.exports = {
+	public: true,
+	date: '9/26/2017',
+	category: 'interview',
+	mapPosition: {
+		location: 'Illutron',
+		bearing: 0,
+		center: [12.61235350000004, 55.6930383],
+		zoom: 12.00,
+		pitch: 0
+	}
+}
