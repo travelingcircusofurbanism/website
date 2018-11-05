f87aed115ba7346c141e1e28efd2d4409f554fc1
Jasper Stephenson <jasperstephenson@gmail.com>
2018-11-05 08:01:09 -0800

    Auto-pushing files to live

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="master" ref="master" :class="{mobile: isMobile}">
+    <Lightbox class="lightbox" />
     <Header />
     <Canvas />
     <nuxt />
@@ -7,59 +8,65 @@
 </template>
 
 <script>
-  import Canvas from '~/components/Canvas'
-  import Header from '~/components/Header'
+import Canvas from '~/components/Canvas'
+import Header from '~/components/Header'
+import Lightbox from '~/components/Lightbox'
 
-  export default {
-    components: { Canvas, Header },
-    computed: {
-      isMobile () { return this.$store.state.isMobile },
+export default {
+  components: { Canvas, Header, Lightbox },
+  computed: {
+    isMobile() {
+      return this.$store.state.isMobile
     },
-    mounted() {
-      this.$store.commit('setLanguage', window ? window.navigator.userLanguage || window.navigator.language : 'en_US')
-      if (window) window.addEventListener('resize', this.checkWidth)
-      this.checkWidth()
-      this.$root._router.afterEach(this.resetScroll)
+  },
+  mounted() {
+    this.$store.commit(
+      'setLanguage',
+      window
+        ? window.navigator.userLanguage || window.navigator.language
+        : 'en_US'
+    )
+    if (window) window.addEventListener('resize', this.checkWidth)
+    this.checkWidth()
+    this.$root._router.afterEach(this.resetScroll)
+  },
+  methods: {
+    checkWidth() {
+      this.$store.commit('setMobile', window ? window.innerWidth : 1000)
     },
-    methods: {
-      checkWidth () {
-        this.$store.commit('setMobile', window ? window.innerWidth : 1000)
-      },
-      resetScroll () {
-        document.querySelector('body').scrollTo(0, 0)
-      }
-    }
-  }
-
+    resetScroll() {
+      document.querySelector('body').scrollTo(0, 0)
+    },
+  },
+}
 </script>
 
 <style lang="scss">
 @import '~/assets/variables.scss';
 
-  .master {
-    display: flex;
+.master {
+  display: flex;
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+
+  & > *:not(.lightbox) {
+    flex: 1;
+    max-width: 50%;
     overflow: hidden;
-    width: 100vw;
+    overflow-y: auto;
     height: 100vh;
-
-    & > * {
-      flex: 1;
-      max-width: 50%;
-      overflow: hidden;
-      overflow-y: auto;
-      height: 100vh;
-    }
-
-    @include width (mobile) {
-      display: block;
-      height: auto;
-
-      & > * {
-        max-width: 100%;
-        overflow-y: visible;
-        height: auto;
-      }
-    }
   }
 
+  @include width(mobile) {
+    display: block;
+    height: auto;
+
+    & > * {
+      max-width: 100%;
+      overflow-y: visible;
+      height: auto;
+    }
+  }
+}
 </style>
