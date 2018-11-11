03b528026bdc6a281a073abdfc7c1b1894f5a5f3
Jasper Stephenson <jasperstephenson@gmail.com>
2018-11-10 22:41:47 -0800

    Auto-pushing files to live

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="content">
-  <div class="intro" v-if="showIntro">
+    <!--<div class="intro" v-if="showIntro">
       <strong>This is a blog by <nuxt-link to="/author" exact class="subtle">Mariko Sugita</nuxt-link>,</strong> city enthusiast and nomadic urban researcher. It's about urban culture, design, planning, history, spaces, and more – <strong>all about cities!</strong>
-    </div>
+    </div>-->
     <div class="content-top-full mini gray">
       <Selector />
       <Selector
@@ -27,71 +27,80 @@ import Selector from '~/components/Selector'
 const { get, set } = require('~/assets/storage').default
 
 export default {
-  head () { 
-    return { 
+  head() {
+    return {
       title: 'Home',
       meta: [
         { property: 'og:title', content: 'Traveling Circus of Urbanism' },
-        { property: 'og:url', content: `https://www.travelingcircusofurbanism.com` },
-      ]
+        {
+          property: 'og:url',
+          content: `https://www.travelingcircusofurbanism.com`,
+        },
+      ],
     }
   },
-  components: { Footer, PostList, Selector, },
-  asyncData ({ store }) {
-    let posts = store.state.isDev ?
-      store.state.allPosts : 
-      store.state.allPublicPosts
+  components: { Footer, PostList, Selector },
+  asyncData({ store }) {
+    let posts = store.state.isDev
+      ? store.state.allPosts
+      : store.state.allPublicPosts
     return {
       posts,
     }
   },
-  data () {
+  data() {
     return {
       showIntro: false,
     }
   },
   computed: {
-    isMobile () { return this.$store.state.isMobile },
-    isDev () { return this.$store.state.isDev },
-    userLanguage () { return this.$store.state.language },
-		showablePosts () { 
-			return this.posts && this.userLanguage === 'en' ?
-        this.posts.filter(p => p.languages['en'] === true) :
-        this.posts
-		}
+    isMobile() {
+      return this.$store.state.isMobile
+    },
+    isDev() {
+      return this.$store.state.isDev
+    },
+    userLanguage() {
+      return this.$store.state.language
+    },
+    showablePosts() {
+      return this.posts && this.userLanguage === 'en'
+        ? this.posts.filter(p => p.languages['en'] === true)
+        : this.posts
+    },
   },
-  mounted () {
+  mounted() {
     this.$store.commit('setPan', true)
     this.$store.commit('setView', this.showablePosts)
     this.$store.commit('setHighlight')
     if (!this.get('visited')) {
       this.showIntro = true
-      this.set('visited', true)
+      // this.set('visited', true)
     }
   },
   methods: {
-    get, set,
-  }
+    get,
+    set,
+  },
 }
 </script>
 
 <style scoped lang="scss">
 @import '~/assets/variables.scss';
 
-  .intro {
-    margin: $content-padding * -1;
-    padding: $content-padding * 1.5;
-    background: $shade;
-    line-height: 1.4;
-    color: darken($offwhite, 60%);
-    text-align: center;
-    font-size: 1.2rem;
+.intro {
+  margin: $content-padding * -1;
+  padding: $content-padding * 1.5;
+  background: $shade;
+  line-height: 1.4;
+  color: darken($offwhite, 60%);
+  text-align: center;
+  font-size: 1.2rem;
 
-    @include width (mobile) {
-      margin: $content-padding-mobile * -1;
-      margin-bottom: $content-padding-mobile * 1;
-      padding: $content-padding-mobile * 1.5;
-    }
+  @include width(mobile) {
+    margin: $content-padding-mobile * -1;
+    margin-bottom: $content-padding-mobile * 1;
+    padding: $content-padding-mobile * 1.5;
   }
-
+}
 </style>
