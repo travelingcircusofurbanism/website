146e438b29751cdfe52bc8159700382ab128186d
Mariko <mariko9012@gmail.com>
2019-02-13 22:54:12 -0600

    Auto-pushing files to live

---
+++ posts/mexico city/subjectivemapworkshop/data.js
@@ -0,0 +1,13 @@
+module.exports = {
+  public: false,
+  date: '1/31/2019',
+  category: 'place',
+  image: '',
+  mapPosition: {
+    location: 'Audiorama',
+    bearing: 0,
+    center: [-99.181155, 19.419301],
+    zoom: 12.0,
+    pitch: 0,
+  },
+}
