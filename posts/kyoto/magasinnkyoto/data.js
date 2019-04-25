aa96d6a73ca2cc1bd51ddf995792a02ab504b9bb
Mariko <mariko9012@gmail.com>
2019-04-25 16:47:59 +0900

    Auto-pushing files to live

--- posts/kyoto/magasinnkyoto/data.js
+++ posts/kyoto/magasinnkyoto/data.js
@@ -1,7 +1,7 @@
 module.exports = {
   preview: true,
-  public: false,
-  date: '4/20/2019',
+  public: true,
+  date: '4/25/2019',
   category: 'interview',
   image: 'magasinn5.jpg',
   mapPosition: [
@@ -9,6 +9,13 @@ module.exports = {
       location: 'Magasinn Kyoto',
       bearing: 0,
       center: [135.74819, 35.018827],
+      zoom: 12.0,
+      pitch: 0,
+    },
+    {
+      location: 'Kumagusuku',
+      bearing: 0,
+      center: [135.745854, 35.006434],
       zoom: 12.0,
       pitch: 0,
     },
