79c6f0e6146c37e7c27b34457891a419d3759da6
Jasper Stephenson <jasperstephenson@gmail.com>
2019-11-27 08:46:58 -0500

    Added contribution guide

--- nuxt/pages/getinvolved.vue
+++ nuxt/pages/getinvolved.vue
@@ -5,7 +5,11 @@
       <h1 class="title">
         <span class="small">Get Involved!</span>
       </h1>
-      <p>Traveling Circus of Urbanismでは、世界中の実践者からの寄稿を、随時受け付けています。自身の街の実践について、日英バイリンガルで国内外に発信したい方、取材をしてもらいたい、インタビューして欲しいなど、なんでもお気軽にご相談ください。コラボレーションのアイデアもお待ちしています！</p>
+      <p>
+        Traveling Circus of Urbanismでは、世界中の実践者からの寄稿を、随時受け付けています。自身の街の実践について、日英バイリンガルで国内外に発信したい方、取材をしてもらいたい、インタビューして欲しいなど、なんでもお気軽にご相談ください。
+        <!-- <a href="~static/assets/Traveling-Circus-of-Urbanism-Contribution-Guideline.pdf" target="_blank">コントリビューションガイド</a>は用意してるので参考にしてください。 -->
+        コラボレーションのアイデアもお待ちしています！
+      </p>
       <br />
       <a href="mailto:travelingcircusofurbanism@gmail.com" class="nounderline button full">メールを送る</a>
     </div>
@@ -24,8 +28,11 @@
       <p>
         Traveling Circus of Urbanism is always looking to collaborate with city
         enthusiasts across the world. If you are an urbanist who's passionate
-        about cities and you're interested in collaborating or contributing,
-        don’t hesitate to drop us a line!
+        about cities and you're interested in collaborating or contributing, check out our
+        <a
+          href="/assets/Traveling-Circus-of-Urbanism-Contribution-Guideline.pdf"
+          target="_blank"
+        >contribution guide</a>, and don’t hesitate to drop us a line!
       </p>
       <br />
       <a
@@ -38,47 +45,47 @@
 </template>
 
 <script>
-import ContentFooter from "~/components/Footer";
+import ContentFooter from '~/components/Footer'
 export default {
   head() {
     return {
-      title: "Get Involved",
+      title: 'Get Involved',
       meta: [
         {
-          property: "og:title",
-          content: "Get Involved | Traveling Circus of Urbanism"
+          property: 'og:title',
+          content: 'Get Involved | Traveling Circus of Urbanism',
         },
         {
-          property: "og:url",
-          content: `https://www.travelingcircusofurbanism.com/getinvolved/`
-        }
-      ]
-    };
+          property: 'og:url',
+          content: `https://www.travelingcircusofurbanism.com/getinvolved/`,
+        },
+      ],
+    }
   },
   components: { ContentFooter },
   computed: {
     displayLanguage() {
-      return this.$i18n.locale;
-    }
+      return this.$i18n.locale
+    },
   },
   created() {
-    this.$store.commit("setBreadcrumbs", [
+    this.$store.commit('setBreadcrumbs', [
       {
-        label: "Get Involved",
-        url: this.localePath("getinvolved")
-      }
-    ]);
+        label: 'Get Involved',
+        url: this.localePath('getinvolved'),
+      },
+    ])
   },
   mounted() {
     this.$nextTick(() => {
-      this.$store.commit("setHighlight");
-    });
-  }
-};
+      this.$store.commit('setHighlight')
+    })
+  },
+}
 </script>
 
 <style scoped lang="scss">
-@import "~/assets/variables.scss";
+@import '~/assets/variables.scss';
 
 .ja {
   font-size: 0.93em;
