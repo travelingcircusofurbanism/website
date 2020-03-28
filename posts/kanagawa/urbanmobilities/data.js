ca2b8a496b8daff98088a33ad2f614f788dc6309
Mariko <mariko9012@gmail.com>
2020-03-28 23:17:12 +0900

    Auto-pushing files

---
+++ posts/kanagawa/urbanmobilities/data.js
@@ -0,0 +1,15 @@
+module.exports = {
+  public: true,
+  preview: true,
+  date: '3/28/2020',
+  category: 'essay',
+  mapPosition: [
+    {
+      location: 'Kanagawa',
+      bearing: 0,
+      center: [139.487571, 35.338533],
+      zoom: 12.0,
+      pitch: 0,
+    },
+  ],
+}
