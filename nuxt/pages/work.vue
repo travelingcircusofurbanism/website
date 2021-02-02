5b96ae2654bcdec6925141cd07111096bc1476cb
Mariko <mariko9012@gmail.com>
2021-02-02 23:42:33 +0100

    Auto-pushing files

---
+++ nuxt/pages/work.vue
@@ -0,0 +1,237 @@
+<template>
+  <section class="content">
+    <div class="header content-top-full"></div>
+    <div v-if="displayLanguage === 'ja'" class="ja">
+      <h1 class="title">
+        <span class="small">このサイトについて</span>
+      </h1>
+      <p>Traveling Circus of Urbanismは、世界中の都市から様々な街づくりの情報を紹介するサイトです。参考になる街づくりのプロジェクトは日本に限らず海外に沢山あるものの、街づくりの情報サイトは、どうしても１つの地域や都市に限定してしまいがち。そこで、都市、街づくり、建築などのトピックを、グローバルに紹介していくことで、街好きのアーバニスト達が世界中の都市で起こっていることをチェックし、お互いに参考にしあうことが出来るはず。Traveling Circus of Urbanismは、そんな想いで活動しています。</p>
+      <p>まちづくり、都市デザイン、建築、コミュニティディベロップメント、都市のアートや文化、政治、都市のサステイナビリティについてなど、旅をしながら目に触れた都市に関わるトピックを幅広く紹介していきます。</p>
+
+      <SectionHeader>Core Team</SectionHeader>
+      <div class="personlist">
+        <div class="person">
+          <img src="~/assets/staff/marikocolor.jpg" />
+          <div>
+            <span class="name">杉田真理子 Mariko Sugita</span>
+            <span class="title">Founder, Editor in Chief</span>
+            <div>
+              デンマークオーフス大学で都市社会学専攻、その後ブリュッセル自由大学大学院にて、Urban Studies修士号取得。欧州を中心に、現在まで多くの都市・街づくり関連団体を訪れ、参加型調査やワークショップを重ねてきた経験から、参加型街づくりの仕組みづくりやその情報発信を得意とする。都市に関する取材執筆、調査、翻訳、調査成果物やアーカイブシステムの構築など、編集を軸にした活動を行う。
+              <a
+                href="http://www.linkedin.com/in/marikosugita"
+                target="_blank"
+              >LinkedIn</a>,
+              <a href="https://twitter.com/marikosugita" target="_blank">Twitter</a>,
+              <a href="https://www.instagram.com/mariko_urbannomad/" target="_blank">Instagram</a>,
+              <a href="https://www.wantedly.com/users/18183183" target="_blank">Wantedly</a>
+            </div>
+          </div>
+        </div>
+        <div class="person">
+          <img src="~/assets/staff/jaspercolor.jpg" />
+          <div>
+            <span class="name">ジャスパー・スティーベンソン Jasper Stephenson</span>
+            <span class="title">Designer, Developer</span>
+            <div>
+              ノースカロライナ州立大学にて、グラフィックデザインを専攻。サンフランシスコやオースティンなどでデザイン関係の職を経験したのち、2015年に拠点を日本に移す。2017年、株式会社Cygamesに入社。プロトタイプや開発に関わる部署にて、UXエンジニアとして働く。2018年から、フリーへ。サンフランシスコのAdobeなどを中心に、UXに関わるプロジェクトに多く携わる。
+              <a
+                href="https://www.jasperstephenson.com"
+                target="_blank"
+              >Website</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <template v-else>
+      <h1 class="title">What is Traveling Circus of Urbanism?</h1>
+      <p>Traveling Circus of Urbanism is a platform for urban narratives that are collected from the world through traveling and dialogues with the citizens of the world.</p>
+
+      <p>As an urbanist and city enthusiast, I’ve visited cities around the world. The more places I went, the more it became clear: I needed a way to share and archive my overwhelming collection of experiences and urban realities — after all, communicating the knowledge gained through research is as important as the knowledge itself.</p>
+
+      <p>During the graduation ceremony of my trans-Europe master program, a professor jokingly described our cohort as “a traveling circus of urbanism” because of our peculiar brand of roaming urban study. That phrase stuck in my head, and a few years later, Traveling Circus of Urbanism was born. Travel, both physically and mentally, is a way to unravel the essence of places, seeing them from different perspectives and revealing how they are globally connected. A circus moves from a city to another, telling stories that are collected from its journey. Urbanism is about how people live in cities and interact with our urban environments.</p>
+
+      <p>
+        It started humbly as a website, a platform to collect urban narratives through my journalistic travel across the cities in the world. Soon, fellow urbanists from all over the world began offering contributions to share incredible stories about their cities. We’ve grown from there, from magazines to even an “urbanist in residency” program called
+        <a
+          href="https://www.brdg.to/"
+          target="_blank"
+        >Bridge To</a>.
+      </p>
+
+      <p>Traveling Circus of Urbanism puts forward a simple but contentious concept — namely, the need for more global perspectives in investigations of cities, and for urban storytelling to side with the underrepresented.</p>
+
+      <p>
+        I hope Traveling Circus of Urbanism can help to bring the urbanists of the world together, beyond professions, disciplines, and nationalities. If you have a story from your city, you can
+        <nuxt-link to="/contribute">contribute it</nuxt-link>!
+      </p>
+
+      <p>
+        <i>–Mariko Sugita</i>
+      </p>
+
+      <SectionHeader>Core Team</SectionHeader>
+      <div class="personlist">
+        <div class="person">
+          <img src="~/assets/staff/marikocolor.jpg" />
+          <div>
+            <span class="name">Mariko Sugita</span>
+            <span class="job">Founder, Editor in Chief</span>
+            <div>
+              Mariko is an urbanist and city lover. Her professional fields are
+              research, marketing, and editing and writing in the field of
+              architecture and urbanism.
+              <a
+                href="http://www.linkedin.com/in/marikosugita"
+                target="_blank"
+              >Full profile</a>
+            </div>
+          </div>
+        </div>
+        <div class="person">
+          <img src="~/assets/staff/jaspercolor.jpg" />
+          <div>
+            <span class="name">Jasper Stephenson</span>
+            <span class="job">Designer, Developer</span>
+            <div>
+              Jasper is a traveler and creative polymath. He spends his free
+              time learning languages and designing and developing his latest
+              passion projects.
+              <a
+                href="https://www.jasperstephenson.com"
+                target="_blank"
+              >Website</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <center>
+        Also, thanks to Ramon Bauer and
+        <a href="https://www.4cities.eu/" target="_blank">4CITIES</a> for the name inspiration!
+      </center>
+    </template>
+
+    <ContentFooter />
+  </section>
+</template>
+
+<script>
+import ContentFooter from '~/components/Footer'
+import SectionHeader from '~/components/SectionHeader'
+
+export default {
+  head() {
+    return {
+      title: 'About',
+      meta: [
+        { property: 'og:title', content: 'About Traveling Circus of Urbanism' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'This is an exploration of urban practices with a global perspective, to challenge the focus on localism in the world.',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'This is an exploration of urban practices with a global perspective, to challenge the focus on localism in the world.',
+        },
+        {
+          property: 'og:url',
+          content: `https://www.travelingcircusofurbanism.com/about/`,
+        },
+      ],
+    }
+  },
+  components: { ContentFooter, SectionHeader },
+  computed: {
+    displayLanguage() {
+      return this.$i18n.locale
+    },
+  },
+  created() {
+    this.$store.commit('setBreadcrumbs', [
+      {
+        label: 'About',
+        url: this.localePath('about'),
+      },
+    ])
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$store.commit('setHighlight')
+    })
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '~/assets/variables.scss';
+
+.header {
+  background-image: url('~assets/about.jpg');
+  background-size: cover;
+  background-position: center center;
+  height: 30vh;
+}
+
+.ja {
+  font-size: 0.93em;
+  line-height: 1.85;
+  letter-spacing: 0.03em;
+}
+
+img {
+  max-width: 100% !important;
+  display: block;
+  margin: 0 auto;
+}
+
+.small {
+  font-size: 0.6em;
+}
+// .personlist {
+//   // display: flex;
+//   // flex-wrap: wrap;
+//   // justify-content: center;
+// }
+.person {
+  $img-height: $unit * 18;
+  display: inline-block;
+  margin: 0 0 $unit * 8 0;
+  display: grid;
+  grid-template-columns: $img-height 1fr;
+  grid-gap: $unit * 4;
+
+  @include width(mobile) {
+    grid-template-columns: 1fr;
+    text-align: center;
+  }
+
+  & > img {
+    width: $img-height;
+    height: $img-height;
+    border-radius: $img-height / 2;
+    @include width(mobile) {
+      margin: 0 auto;
+    }
+  }
+
+  & > span {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
+.name {
+  font-weight: 700;
+}
+.job {
+  font-weight: 500;
+  opacity: 0.3;
+  margin-bottom: $unit;
+}
+</style>
--- posts/amsterdam/Introduction/en.md
+++ posts/amsterdam/Introduction/en.md
@@ -1,41 +1,42 @@
 
-# TUrbanist's Guide to Amsterdam
+# Urbanist's Guide to Amsterdam
 
 ![](amsterdam01.jpg)
 *Photo by Aquiles Carattino on Unsplash*
 
+I've moved to Amsterdam from Kyoto last year, in midst of the pandemic. It's been an interesting journey with ups and downs, but I've still managed to encounter some initiatives and places that I love to share. Hopefully the world would get back to normal in the near future, where we can start enjoying strolling cities again. 
+
 ### Things to do, places to go
 
-- [De Cuevel](https://deceuvel.nl/en/)
+- [De Cuevel](https://deceuvel.nl/en/) is an urban 'playground' for the exploration and testing of new green technologies and social enterprises adjacent to the van Hasselt kanaal off the river IJ in Amsterdam North. The site was formerly a derelict and polluted shipyard, which then renovated into a regenerative urban oasis. You see many small, creative offices using the old, recycled ships and boat here, and it's always fun to walk around here. 
 
-- [De School](https://www.deschoolamsterdam.nl/en/)
+- [De School](https://www.deschoolamsterdam.nl/en/) used to be a technical school, and is famous for a nightclub - it also has a concert venue, a restaurant, a café, an exhibition space and a gym. It hosts a lot of events there too. 
 
-- [BRET](https://www.bret.bar/) is Restaurant & Club 
+- I had an oppotunity to have a chat with [Martijn de Waal](http://martijndewaal.nl/), a lecturer of Play and Civic Media at the Amsterdam University of Applied Sciences and a director of [Hackable City](http://thehackablecity.nl/). The Hackable City is a research project that explores the potential for new modes of collaborative citymaking, set in Buiksloterham, a brownfield regeneration project in Amsterdam North. He also runs [Media Architecture Institute](https://www.mediaarchitecture.org/), which is worth checking out. 
 
-- Hackable City
-Martijn de Waal - New Media and Public Space (Amsterdam University of Applied Sciences) 
-Media Architecture Biennale 2020: https://mab20.mediaarchitecture.org/
-Media Architecture Institute: https://www.mediaarchitecture.org/
+- I've been always curious about [A City Made By People](https://www.acitymadebypeople.com/). They are a creative platform for city enthusiasts, and they organize various events, workshops, talks and publications to encourage people to become better citizens. 
 
-- [A City Made By People](https://www.acitymadebypeople.com/)
+- Who doesn't wanna taste the flavour of Amsterdam? I found [Chateau Amsterdam](https://chateau.amsterdam/en/) right next to [Studio KU+](https://www.studiokuplus.com/), an architectural model workshop based in Amsterdam. Chateau Amsterdam produces some great 'urban' wine, by collaboration with farmers from all over Europe. Wine made in the city, for the city. 
 
-- [Chateau Amsterdam](https://chateau.amsterdam/en/)
+- Speaking of a beverage, [BRET](https://www.bret.bar/) is a great restaurant & club all about beer, sustainability and good music. Don't forget to take a look at [PIT Lab](https://doorarchitecten.nl/pit-lab/) next door - the container building structure is creative and playful. 
+
+- [Mediamatic](https://www.mediamatic.net/) is an art centre dedicated to new developments in the arts since 1983. They organize lectures, workshops and art projects, focusing on nature, biotechnology and art+science in a strong international network.
 
 ### Read
 
-- Failed Architecture
+- [Volume](http://volumeproject.org/) is an independent magazine that sets the agenda for architecture and design. It's fairly famous in Japan, and I've been a fan for a long time. 
 
-- Volume
+- Aside from Valume, my favorite is [Failed Architecture](https://failedarchitecture.com/). "Architecture continues to fail because we are stuck in a global economic system which puts profit above everything else" they say, thus the name Failed Architecture. Their [Podcast](https://failedarchitecture.com/podcast/) is worth listening to as well. 
 
 ### Organization / initiative
 
 - Amsterdam-based [CASCOLAND](http://cascoland.com/#/)is an international network of artists, architects, designers and performers. They organize various interdisciplinary interventions in public space, aiming at the development of an ecological and socially sustainable society.
 
-- [Studio Monnik](https://www.monnik.org/)
+- A strategic think tank[Studio Monnik](https://www.monnik.org/) inspired me a lot when they published the book [Tokyo Totem](http://www.tokyototem.jp/). Nowadays they focus on future scenario making. 
+
+- I visited a beautiful community garden run by [I can change the world with my two hands](https://www.icanchangetheworldwithmytwohands.nl/) in the western part of Amsterdam. I even became a member, and I bring my organic waste once in a while to their compost station, There are many urban gardening initiative out there in Amsterdam, but I believe this is one of the most open, participatory projects run by a strong, highky motivated community. 
 
 - [Waag](https://waag.org/en/home) and [Citizen Science Lab]()
-
-- [I can change the world with my two hands](https://www.icanchangetheworldwithmytwohands.nl/)
 
 - [Placemakers](https://placemakers.nl/en/)
 
@@ -43,9 +44,9 @@ Media Architecture Institute: https://www.mediaarchitecture.org/
 
 - [Pop Up City](https://popupcity.net/)
 
-* Other cities in the Netherlands
+- I live in a boathouse in the Amsterdam West, so I observe the canal water everyday - and it makes me feel deeply sad to see that there are often a lot of plastic trash floating. This is how I discovered [Plastic Whale](https://plasticwhale.com/), the first plastic fishing company in the world. They collect plastic from the canal with boats, and recycle them to make the products with them. They offer various packages for companies to organise a plastic finishing, and 450 different companies have already gone plastic fishing with then. 
 
-- [Independent School for the City](https://www.schoolforthecity.nl/)
+#### Other cities in the Netherlands
 
-- Trust in Play
+- [Independent School for the City](https://www.schoolforthecity.nl/) is a Rotterdam based one-year educational program for post-graduate students from all over the world, and offers a range of classes, lectures, and programs all about cities and urbanism. 
 
