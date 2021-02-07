30779d87748578bab11f0881e32fe86ccf3e0f21
Mariko <mariko9012@gmail.com>
2021-02-07 15:50:30 +0100

    Auto-pushing files

---
+++ posts/tel aviv/streetfurniture/data.js
@@ -0,0 +1,16 @@
+module.exports = {
+  public: false,
+  preview: true,
+  date: '2/7/2021',
+  image: 'bench01.jpg',
+  category: 'essay',
+  mapPosition: [
+    {
+      location: 'Tel Aviv',
+      bearing: 0,
+      center: [34.777673075187934, 32.06987612619526],
+      zoom: 12.0,
+      pitch: 0,
+    },
+  ],
+}
