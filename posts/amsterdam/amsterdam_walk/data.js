11363497221478866b1c59ef0c926b2a3e104b78
Mariko <mariko9012@gmail.com>
2021-03-10 22:12:23 +0100

    Auto-pushing files

---
+++ posts/amsterdam/amsterdam_walk/data.js
@@ -0,0 +1,18 @@
+module.exports = {
+  public: false,
+  preview: true,
+  date: '11/3/2021',
+  category: 'essay',
+  image: 'AmsterdamWalk_ADAFFLaurels.jpg',
+  mapPosition: [
+    {
+      location: '',
+      bearing: 0,
+      center: [4.8939142564095475, 52.372711387677406],
+      zoom: 12.0,
+      pitch: 0,
+    },
+    
+  ], 
+}
+
