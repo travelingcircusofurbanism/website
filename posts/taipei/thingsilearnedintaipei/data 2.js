86a9635d639ae0765d6c7b7d9b646461b1b9e449
Mariko <mariko9012@gmail.com>
2021-03-11 18:18:43 +0100

    update

---
+++ posts/taipei/thingsilearnedintaipei/data 2.js
@@ -0,0 +1,35 @@
+module.exports = {
+  public: false,
+  date: '8/4/2018',
+  category: 'place',
+  mapPosition: [
+    {
+      location: 'Shintomicho Cultural Market',
+      bearing: 0,
+      center: [121.501863, 25.03577],
+      zoom: 12.0,
+      pitch: 0,
+    },
+    {
+      location: 'Taiwan Contemporary Culture Lab (C-LAB)',
+      bearing: 0,
+      center: [121.537998, 25.040655],
+      zoom: 12.0,
+      pitch: 0,
+    },
+    {
+      location: '玖樓 9floor',
+      bearing: 0,
+      center: [121.503191, 25.034999],
+      zoom: 12.0,
+      pitch: 0,
+    },
+    {
+      location: 'Four Four South Village',
+      bearing: 0,
+      center: [121.561938, 25.031347],
+      zoom: 12.0,
+      pitch: 0,
+    },
+  ],
+}
