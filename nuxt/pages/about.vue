f8296ce4d6039dc8fb11457060368a3e4013f285
Jasper Stephenson <jasperstephenson@gmail.com>
2021-06-02 14:59:13 +0900

    ko-fi

--- nuxt/pages/about.vue
+++ nuxt/pages/about.vue
@@ -5,39 +5,67 @@
       <h1 class="title">
         <span class="small">このサイトについて</span>
       </h1>
-      <p>Traveling Circus of Urbanismは、世界中の都市から様々な街づくりの情報を紹介するサイトです。参考になる街づくりのプロジェクトは日本に限らず海外に沢山あるものの、街づくりの情報サイトは、どうしても１つの地域や都市に限定してしまいがち。そこで、都市、街づくり、建築などのトピックを、グローバルに紹介していくことで、街好きのアーバニスト達が世界中の都市で起こっていることをチェックし、お互いに参考にしあうことが出来るはず。Traveling Circus of Urbanismは、そんな想いで活動しています。</p>
-      <p>まちづくり、都市デザイン、建築、コミュニティディベロップメント、都市のアートや文化、政治、都市のサステイナビリティについてなど、旅をしながら目に触れた都市に関わるトピックを幅広く紹介していきます。</p>
+      <p>
+        Traveling Circus of
+        Urbanismは、世界中の都市から様々な街づくりの情報を紹介するサイトです。参考になる街づくりのプロジェクトは日本に限らず海外に沢山あるものの、街づくりの情報サイトは、どうしても１つの地域や都市に限定してしまいがち。そこで、都市、街づくり、建築などのトピックを、グローバルに紹介していくことで、街好きのアーバニスト達が世界中の都市で起こっていることをチェックし、お互いに参考にしあうことが出来るはず。Traveling
+        Circus of Urbanismは、そんな想いで活動しています。
+      </p>
+      <p>
+        まちづくり、都市デザイン、建築、コミュニティディベロップメント、都市のアートや文化、政治、都市のサステイナビリティについてなど、旅をしながら目に触れた都市に関わるトピックを幅広く紹介していきます。
+      </p>
 
       <SectionHeader>Core Team</SectionHeader>
       <div class="personlist">
         <div class="person">
           <img src="~/assets/staff/marikocolor.jpg" />
           <div>
-            <span class="name">杉田真理子 Mariko Sugita</span>
-            <span class="title">Founder, Editor in Chief</span>
+            <span class="name"
+              >杉田真理子 Mariko Sugita</span
+            >
+            <span class="title"
+              >Founder, Editor in Chief</span
+            >
             <div>
-              デンマークオーフス大学で都市社会学専攻、その後ブリュッセル自由大学大学院にて、Urban Studies修士号取得。欧州を中心に、現在まで多くの都市・街づくり関連団体を訪れ、参加型調査やワークショップを重ねてきた経験から、参加型街づくりの仕組みづくりやその情報発信を得意とする。都市に関する取材執筆、調査、翻訳、調査成果物やアーカイブシステムの構築など、編集を軸にした活動を行う。
+              デンマークオーフス大学で都市社会学専攻、その後ブリュッセル自由大学大学院にて、Urban
+              Studies修士号取得。欧州を中心に、現在まで多くの都市・街づくり関連団体を訪れ、参加型調査やワークショップを重ねてきた経験から、参加型街づくりの仕組みづくりやその情報発信を得意とする。都市に関する取材執筆、調査、翻訳、調査成果物やアーカイブシステムの構築など、編集を軸にした活動を行う。
               <a
                 href="http://www.linkedin.com/in/marikosugita"
                 target="_blank"
-              >LinkedIn</a>,
-              <a href="https://twitter.com/marikosugita" target="_blank">Twitter</a>,
-              <a href="https://www.instagram.com/mariko_urbannomad/" target="_blank">Instagram</a>,
-              <a href="https://www.wantedly.com/users/18183183" target="_blank">Wantedly</a>
+                >LinkedIn</a
+              >,
+              <a
+                href="https://twitter.com/marikosugita"
+                target="_blank"
+                >Twitter</a
+              >,
+              <a
+                href="https://www.instagram.com/mariko_urbannomad/"
+                target="_blank"
+                >Instagram</a
+              >,
+              <a
+                href="https://www.wantedly.com/users/18183183"
+                target="_blank"
+                >Wantedly</a
+              >
             </div>
           </div>
         </div>
         <div class="person">
           <img src="~/assets/staff/jaspercolor.jpg" />
           <div>
-            <span class="name">ジャスパー・スティーベンソン Jasper Stephenson</span>
+            <span class="name"
+              >ジャスパー・スティーベンソン Jasper
+              Stephenson</span
+            >
             <span class="title">Designer, Developer</span>
             <div>
               ノースカロライナ州立大学にて、グラフィックデザインを専攻。サンフランシスコやオースティンなどでデザイン関係の職を経験したのち、2015年に拠点を日本に移す。2017年、株式会社Cygamesに入社。プロトタイプや開発に関わる部署にて、UXエンジニアとして働く。2018年から、フリーへ。サンフランシスコのAdobeなどを中心に、UXに関わるプロジェクトに多く携わる。
               <a
                 href="https://www.jasperstephenson.com"
                 target="_blank"
-              >Website</a>
+                >Website</a
+              >
             </div>
           </div>
         </div>
@@ -45,26 +73,70 @@
     </div>
 
     <template v-else>
-      <h1 class="title">What is Traveling Circus of Urbanism?</h1>
-      <p>Traveling Circus of Urbanism is a platform for urban narratives that are collected from the world through traveling and dialogues with the citizens of the world.</p>
-
-      <p>As an urbanist and city enthusiast, I’ve visited cities around the world. The more places I went, the more it became clear: I needed a way to share and archive my overwhelming collection of experiences and urban realities — after all, communicating the knowledge gained through research is as important as the knowledge itself.</p>
-
-      <p>During the graduation ceremony of my trans-Europe master program, a professor jokingly described our cohort as “a traveling circus of urbanism” because of our peculiar brand of roaming urban study. That phrase stuck in my head, and a few years later, Traveling Circus of Urbanism was born. Travel, both physically and mentally, is a way to unravel the essence of places, seeing them from different perspectives and revealing how they are globally connected. A circus moves from a city to another, telling stories that are collected from its journey. Urbanism is about how people live in cities and interact with our urban environments.</p>
-
+      <h1 class="title">
+        What is Traveling Circus of Urbanism?
+      </h1>
       <p>
-        It started humbly as a website, a platform to collect urban narratives through my journalistic travel across the cities in the world. Soon, fellow urbanists from all over the world began offering contributions to share incredible stories about their cities. We’ve grown from there, from magazines to even an “urbanist in residency” program called
-        <a
-          href="https://www.brdg.to/"
-          target="_blank"
-        >Bridge To</a>.
+        Traveling Circus of Urbanism is a platform for urban
+        narratives that are collected from the world through
+        traveling and dialogues with the citizens of the
+        world.
       </p>
 
-      <p>Traveling Circus of Urbanism puts forward a simple but contentious concept — namely, the need for more global perspectives in investigations of cities, and for urban storytelling to side with the underrepresented.</p>
+      <p>
+        As an urbanist and city enthusiast, I’ve visited
+        cities around the world. The more places I went, the
+        more it became clear: I needed a way to share and
+        archive my overwhelming collection of experiences
+        and urban realities — after all, communicating the
+        knowledge gained through research is as important as
+        the knowledge itself.
+      </p>
 
       <p>
-        I hope Traveling Circus of Urbanism can help to bring the urbanists of the world together, beyond professions, disciplines, and nationalities. If you have a story from your city, you can
-        <nuxt-link to="/contribute">contribute it</nuxt-link>!
+        During the graduation ceremony of my trans-Europe
+        master program, a professor jokingly described our
+        cohort as “a traveling circus of urbanism” because
+        of our peculiar brand of roaming urban study. That
+        phrase stuck in my head, and a few years later,
+        Traveling Circus of Urbanism was born. Travel, both
+        physically and mentally, is a way to unravel the
+        essence of places, seeing them from different
+        perspectives and revealing how they are globally
+        connected. A circus moves from a city to another,
+        telling stories that are collected from its journey.
+        Urbanism is about how people live in cities and
+        interact with our urban environments.
+      </p>
+
+      <p>
+        It started humbly as a website, a platform to
+        collect urban narratives through my journalistic
+        travel across the cities in the world. Soon, fellow
+        urbanists from all over the world began offering
+        contributions to share incredible stories about
+        their cities. We’ve grown from there, from magazines
+        to even an “urbanist in residency” program called
+        <a href="https://www.brdg.to/" target="_blank"
+          >Bridge To</a
+        >.
+      </p>
+
+      <p>
+        Traveling Circus of Urbanism puts forward a simple
+        but contentious concept — namely, the need for more
+        global perspectives in investigations of cities, and
+        for urban storytelling to side with the
+        underrepresented.
+      </p>
+
+      <p>
+        I hope Traveling Circus of Urbanism can help to
+        bring the urbanists of the world together, beyond
+        professions, disciplines, and nationalities. If you
+        have a story from your city, you can
+        <nuxt-link to="/contribute">contribute it</nuxt-link
+        >!
       </p>
 
       <p>
@@ -77,15 +149,19 @@
           <img src="~/assets/staff/marikocolor.jpg" />
           <div>
             <span class="name">Mariko Sugita</span>
-            <span class="job">Founder, Editor in Chief</span>
+            <span class="job"
+              >Founder, Editor in Chief</span
+            >
             <div>
-              Mariko is an urbanist and city lover. Her professional fields are
-              research, marketing, and editing and writing in the field of
+              Mariko is an urbanist and city lover. Her
+              professional fields are research, marketing,
+              and editing and writing in the field of
               architecture and urbanism.
               <a
                 href="http://www.linkedin.com/in/marikosugita"
                 target="_blank"
-              >Full profile</a>
+                >Full profile</a
+              >
             </div>
           </div>
         </div>
@@ -95,13 +171,15 @@
             <span class="name">Jasper Stephenson</span>
             <span class="job">Designer, Developer</span>
             <div>
-              Jasper is a traveler and creative polymath. He spends his free
-              time learning languages and designing and developing his latest
-              passion projects.
+              Jasper is a traveler and creative polymath. He
+              spends his free time learning languages and
+              designing and developing his latest passion
+              projects.
               <a
                 href="https://www.jasperstephenson.com"
                 target="_blank"
-              >Website</a>
+                >Website</a
+              >
             </div>
           </div>
         </div>
@@ -109,9 +187,19 @@
       <br />
       <center>
         Also, thanks to Ramon Bauer and
-        <a href="https://www.4cities.eu/" target="_blank">4CITIES</a> for the name inspiration!
+        <a href="https://www.4cities.eu/" target="_blank"
+          >4CITIES</a
+        >
+        for the name inspiration!
       </center>
     </template>
+
+    <iframe
+      class="kofi"
+      src="https://ko-fi.com/travelingcircusofurbanism/?hidefeed=true&widget=true&embed=true&preview=true"
+      height="712"
+      title="travelingcircusofurbanism"
+    ></iframe>
 
     <ContentFooter />
   </section>
@@ -126,7 +214,10 @@ export default {
     return {
       title: 'About',
       meta: [
-        { property: 'og:title', content: 'About Traveling Circus of Urbanism' },
+        {
+          property: 'og:title',
+          content: 'About Traveling Circus of Urbanism',
+        },
         {
           hid: 'description',
           name: 'description',
@@ -233,5 +324,13 @@ img {
   font-weight: 500;
   opacity: 0.3;
   margin-bottom: $unit;
+}
+
+.kofi {
+  margin-top: 2em;
+  border: 1px solid #ddd;
+  width: 100%;
+  padding: 4px;
+  background: #f9f9f9;
 }
 </style>
--- nuxt/pages/contribute.vue
+++ nuxt/pages/contribute.vue
@@ -7,23 +7,34 @@
       </h1>
       <p>
         Traveling Circus of
-        Urbanismでは、世界中の実践者からの寄稿を、随時受け付けています。自身の街の実践について、日英バイリンガルで国内外に発信したい方、取材をしてもらいたい、インタビューして欲しいなど、なんでもお気軽にご連絡ください。コラボレーションのアイデアもお待ちしています。メールは<a href="mailto:travelingcircusofurbanism@gmail.com">こちら</a>まで。
+        Urbanismでは、世界中の実践者からの寄稿を、随時受け付けています。自身の街の実践について、日英バイリンガルで国内外に発信したい方、取材をしてもらいたい、インタビューして欲しいなど、なんでもお気軽にご連絡ください。コラボレーションのアイデアもお待ちしています。メールは<a
+          href="mailto:travelingcircusofurbanism@gmail.com"
+          >こちら</a
+        >まで。
       </p>
 
       <h3>
-  エッセイを寄稿する
-</h3>
-<p>
+        エッセイを寄稿する
+      </h3>
+      <p>
+        都市・建築・まちづくりに関するさまざまな記事の寄稿を受け付けています。学術的な内容でも、主観を織り交ぜたエッセイでも、あなたの街のおすすめスポットの紹介など、形式は問いません。英語・日本語どちらの言語でも受け付けています。コントリビューションガイドは<a
+          href="~static/assets/Traveling-Circus-of-Urbanism-Contribution-Guideline.pdf"
+          target="_blank"
+          >こちら（英語）</a
+        >。
+      </p>
 
-都市・建築・まちづくりに関するさまざまな記事の寄稿を受け付けています。学術的な内容でも、主観を織り交ぜたエッセイでも、あなたの街のおすすめスポットの紹介など、形式は問いません。英語・日本語どちらの言語でも受け付けています。コントリビューションガイドは<a href="~static/assets/Traveling-Circus-of-Urbanism-Contribution-Guideline.pdf" target="_blank">こちら（英語）</a>。
-</p>
-
-<h3>
-  リンクを投稿する
-</h3>
-<p>
-  Traveling Circus of Urbanismでは、都市に関する世界中のプロジェクトやニュースが地図上で閲覧できるようになっています。記事を書くのは難しいければど、地図上でプロジェクトの紹介をしたいという場合は、HPのリンクを<a href="mailto:travelingcircusofurbanism@gmail.com">メール</a>にてご送付ください。HPへ移行するロケーションピンをTraveling Circus of Urbanismの地図上に追加します。
-</p>
+      <h3>
+        リンクを投稿する
+      </h3>
+      <p>
+        Traveling Circus of
+        Urbanismでは、都市に関する世界中のプロジェクトやニュースが地図上で閲覧できるようになっています。記事を書くのは難しいければど、地図上でプロジェクトの紹介をしたいという場合は、HPのリンクを<a
+          href="mailto:travelingcircusofurbanism@gmail.com"
+          >メール</a
+        >にてご送付ください。HPへ移行するロケーションピンをTraveling
+        Circus of Urbanismの地図上に追加します。
+      </p>
 
       <EmailCapture />
 
@@ -37,24 +48,31 @@
     <template v-else>
       <h1 class="title">Contribute</h1>
       <p>
-        Traveling Circus of Urbanism is always looking to collaborate with city
-        enthusiasts across the world, to collect stories, knowledge, and
-        insights for a better understanding of cities. If you are an urbanist
-        who's passionate about cities and you're interested in collaborating,
-        please send us an
-        <a href="mailto:travelingcircusofurbanism@gmail.com">email</a>.
+        Traveling Circus of Urbanism is always looking to
+        collaborate with city enthusiasts across the world,
+        to collect stories, knowledge, and insights for a
+        better understanding of cities. If you are an
+        urbanist who's passionate about cities and you're
+        interested in collaborating, please send us an
+        <a href="mailto:travelingcircusofurbanism@gmail.com"
+          >email</a
+        >.
       </p>
 
-<h3>
-  Send us your essays
-</h3>
+      <h3>
+        Send us your essays
+      </h3>
       <p>
-        We are interested in any stories that deal with the theme of ‘the city’.
-        They could be personal, observational, or documentations of reality.
-        We’re interested in the projects and research you’ve been involved with,
-        the things that represent your life in the city, the things you love or
-        are resentful of in your city, and any other topics that are helpful to
-        get a glimpse of your city. If you want to promote an article that has been already published somewhere, simply send us a link.
+        We are interested in any stories that deal with the
+        theme of ‘the city’. They could be personal,
+        observational, or documentations of reality. We’re
+        interested in the projects and research you’ve been
+        involved with, the things that represent your life
+        in the city, the things you love or are resentful of
+        in your city, and any other topics that are helpful
+        to get a glimpse of your city. If you want to
+        promote an article that has been already published
+        somewhere, simply send us a link.
       </p>
 
       <p>
@@ -63,26 +81,37 @@
           href="/assets/Traveling-Circus-of-Urbanism-Contribution-Guideline.pdf"
           target="_blank"
           >here</a
-        >, and if you have any questions, whether about the writing style,
-        topics, etc, please don’t hesitate to
-        <a href="mailto:travelingcircusofurbanism@gmail.com">drop us a line</a>!
-        We usually reply within a week.
+        >, and if you have any questions, whether about the
+        writing style, topics, etc, please don’t hesitate to
+        <a href="mailto:travelingcircusofurbanism@gmail.com"
+          >drop us a line</a
+        >! We usually reply within a week.
       </p>
 
-<h3>
-  Send us links to your projects 
-</h3>
+      <h3>
+        Send us links to your projects
+      </h3>
 
-<p>
-Traveling Circus of Urbanism provides a world map of stories, initiatives, and happeningd around the theme of urbanism. If you want your project to be included in the map but don't have the time to write a whole article, please
-        <a href="mailto:travelingcircusofurbanism@gmail.com">email us a link</a> to your website explaining the project. We'll create a location pin on the map that goes to your website. </p>
+      <p>
+        Traveling Circus of Urbanism provides a world map of
+        stories, initiatives, and happeningd around the
+        theme of urbanism. If you want your project to be
+        included in the map but don't have the time to write
+        a whole article, please
+        <a href="mailto:travelingcircusofurbanism@gmail.com"
+          >email us a link</a
+        >
+        to your website explaining the project. We'll create
+        a location pin on the map that goes to your website.
+      </p>
 
       <EmailCapture />
 
       <p>
         <b>
           Email:
-          <a href="mailto:travelingcircusofurbanism@gmail.com"
+          <a
+            href="mailto:travelingcircusofurbanism@gmail.com"
             >travelingcircusofurbanism@gmail.com</a
           >
         </b>
@@ -98,6 +127,14 @@ Traveling Circus of Urbanism provides a world map of stories, initiatives, and h
         </b>
       </p>
     </template>
+
+    <iframe
+      class="kofi"
+      src="https://ko-fi.com/travelingcircusofurbanism/?hidefeed=true&widget=true&embed=true&preview=true"
+      height="712"
+      title="travelingcircusofurbanism"
+    ></iframe>
+
     <ContentFooter />
   </section>
 </template>
@@ -112,7 +149,8 @@ export default {
       meta: [
         {
           property: 'og:title',
-          content: 'Contribute | Traveling Circus of Urbanism',
+          content:
+            'Contribute | Traveling Circus of Urbanism',
         },
         {
           property: 'og:url',
@@ -178,5 +216,12 @@ img {
     border: 2px solid transparent;
     color: white;
   }
+}
+.kofi {
+  margin-top: 2em;
+  border: 1px solid #ddd;
+  width: 100%;
+  padding: 4px;
+  background: #f9f9f9;
 }
 </style>
