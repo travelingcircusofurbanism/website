1aa637aea5bb93a859b054e1a80e06e6dd1cce86
Mariko <mariko9012@gmail.com>
2021-06-05 15:08:11 +0900

    Auto-pushing to live

--- posts/elsewhere/What we found this month : Urban projects and news/data.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	public: true,
-	date: '6/26/2018',
-	category: 'essay',
-	image: 'game03.jpg'
-}
---
+++ posts/elsewhere/whatwefoundthisweek01/data.js
@@ -0,0 +1,24 @@
+module.exports = {
+	public: true,
+	date: '6/4/2021',
+	category: 'links',
+	image: 'lysander-yuen-898fT0UnVKY-unsplash.jpeg',
+	mapPosition: [
+		{
+			location: 'Cluster Cairo',
+			bearing: 0,
+			center: [31.24271655767169, 30.05225148839339],
+			zoom: 12.00,
+			pitch: 0
+		},
+		{
+			location: 'HÏSOM',
+			bearing: 0,
+			center: [132.35514692399482, 35.10420400324958],
+			zoom: 12.00,
+			pitch: 0
+		},
+	]
+}
+
+
