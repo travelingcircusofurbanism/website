71fb9228469575dabbf4f17e56c0d16b8ed7671c
mariko9012 <mariko9012@users.noreply.github.com>
2021-06-05 06:13:48 +0000

    deploy: 1aa637aea5bb93a859b054e1a80e06e6dd1cce86

--- _nuxt/static/1622765547/at/seaholm ecodistrict/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622765547",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fat\u002Fseaholm%20ecodistrict",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
