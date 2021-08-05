5737cad14d3c6bef44d23f318aba9a52e93c844f
Mariko <mariko9012@gmail.com>
2021-08-05 15:30:06 +0900

    Auto-pushing to live

--- posts/elsewhere/africanstudytrip/data.js
+++ posts/elsewhere/africanstudytrip/data.js
@@ -1,9 +1,9 @@
 module.exports = {
-  public: true,
-  date: '8/2/2021',
-  category: 'open call',
-  image: 'sandy-ravaloniaina-bKsxa157lf4-unsplash.jpeg',
-  mapPosition: [
+	public: true,
+	date: '8/2/2021',
+	category: 'open call',
+	image: 'sandy-ravaloniaina-bKsxa157lf4-unsplash.jpeg',
+	mapPosition: [
 		{
 			location: 'The Civic Tech Innovation Network',
 			bearing: 0,
@@ -18,21 +18,21 @@ module.exports = {
 			zoom: 12.00,
 			pitch: 0
 		},
-    {
+		{
 			location: 'geo.sm',
 			bearing: 0,
 			center: [11.520638272029949, 3.8608699689966337],
 			zoom: 12.00,
 			pitch: 0
 		},
-    {
-			location: 'L\'alternative architecturale',
+		{
+			location: 'L\'africaine d\'architecture',
 			bearing: 0,
 			center: [1.208380185533349, 6.1647334623305925],
 			zoom: 12.00,
 			pitch: 0
 		},
 	],
-  tags: ['travel','africa'],
+	tags: ['travel', 'africa'],
 }
- 
+
