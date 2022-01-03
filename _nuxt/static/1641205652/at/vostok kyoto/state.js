b71b0f6994707df1d042d81b714c82d26a091321
mariko9012 <mariko9012@users.noreply.github.com>
2022-01-03 10:28:17 +0000

    deploy: 675f0bfc888a77aad4ff2bff2d92bedcb033202a

---
+++ _nuxt/static/1641205652/at/vostok kyoto/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641205652",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fat\u002Fvostok%20kyoto",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
