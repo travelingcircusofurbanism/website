29eff06fad347b9f24b162385ee818afd0c70f22
Mariko Sugita <mariko9012@gmail.com>
2023-04-25 14:08:40 +0900

    Auto-pushing to live

---
+++ posts/dublin/navigatingurbanspace/data.js
@@ -0,0 +1,15 @@
+module.exports = {
+  public: true,
+  date: '4/25/2023',
+  category: 'intro',
+  mapPosition: [
+    {
+      location: '',
+      bearing: 0,
+      center: [-6.272826596909558, 53.339696936971464 ],
+      zoom: 12.0,
+      pitch: 0,
+    },
+   
+  ], 
+}
