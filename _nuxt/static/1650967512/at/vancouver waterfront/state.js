33fe785c94e96a1d25a6a7d5ecec7fd0f5d34095
mariko9012 <mariko9012@users.noreply.github.com>
2023-04-25 05:17:05 +0000

    deploy: 29eff06fad347b9f24b162385ee818afd0c70f22

--- _nuxt/static/1650967512/at/vancouver waterfront/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650967512",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fat\u002Fvancouver%20waterfront",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
