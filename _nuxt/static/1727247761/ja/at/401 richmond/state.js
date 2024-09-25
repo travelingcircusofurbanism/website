75b71fb69f1927fc70751eaa2cf4fd9ee5f8efe0
midblue <midblue@users.noreply.github.com>
2024-09-25 07:03:16 +0000

    deploy: 0970031a6f946ab2235aa4b8df92f453a3117b44

---
+++ _nuxt/static/1727247761/ja/at/401 richmond/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1727247761",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fja\u002Fat\u002F401%20richmond",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
